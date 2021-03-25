3c393f9dd093ab4b55b137b91b901c6cd0be4cbe
Akbar <akbar@cybertekschool.com>
2021-03-25 12:44:17 -0400

    Matchers related to Strings

--- src/test/java/day1/HamcrestMatchersIntro.java
+++ src/test/java/day1/HamcrestMatchersIntro.java
@@ -1,6 +1,7 @@
 package day1;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -26,10 +27,51 @@ public class HamcrestMatchersIntro {
         // negative assertion  5+5 is not 11
         assertThat(5+5, not(11) );
         assertThat(5+5, is( not(11) )   );
-        assertThat(5+5, is( not(  equalTo(11)  ) )    );
+        assertThat(5+5, is(not(equalTo(11))));
+
+        // number comparison
+        //        greaterThan()
+        assertThat(5+5, is( greaterThan(9))  );
+        //        lessThan()
+        //        greaterThanOrEqualTo()
+        //        lessThanOrEqualTo()
+    }
+
+
+    @DisplayName("Matchers related to Strings ")
+    @Test
+    public void stringMatchers(){
+
+        String msg = "B21 is learning Hamcrest" ;
+
+        // checking for equality is same as numbers above
+        assertThat(msg , is("B21 is learning Hamcrest") );
+        assertThat(msg , equalTo("B21 is learning Hamcrest") );
+        assertThat(msg , is(equalTo("B21 is learning Hamcrest")) );
+
+        // check if this msg start with B21
+        assertThat(msg, startsWith("B21"));
+        // now do it in case insensitive manner
+        assertThat(msg, startsWithIgnoringCase("b21") );
+        // check if the msg end with rest
+        assertThat(msg, endsWith("rest") );
+
+        // check if msg contains String learning
+        assertThat(msg, containsString("learning"));
+        assertThat(msg, containsStringIgnoringCase("LEARNING"));
+
+
+
+
 
 
 
     }
+
+
+
+
+
+
 
 }
