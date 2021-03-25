bbcdc60946936ff0f01a46ec3c1404892a823fb0
Akbar <akbar@cybertekschool.com>
2021-03-25 16:49:05 -0400

    extend base class to make test class more concise

--- src/test/java/day1/SpartanNoAuthTest.java
+++ src/test/java/day1/SpartanNoAuthTest.java
@@ -3,29 +3,14 @@ package day1;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
+import test_util.SpartanNoAuthBaseTest;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
 
 @DisplayName("Spartan App Get Requests")
-public class SpartanNoAuthTest {
-
-    //http://18.235.32.166:8000/api/hello
-    //http://18.235.32.166:8000/api/spartans
-
-    @BeforeAll
-    public static void init(){
-        // this will set the part of URL at RestAssured
-        RestAssured.baseURI     = "http://18.235.32.166:8000"  ;
-//        RestAssured.port = 8000 ;
-        RestAssured.basePath    = "/api" ;
-    }
-
-    @AfterAll
-    public static void cleanup(){
-        RestAssured.reset();
-    }
-
+public class SpartanNoAuthTest extends SpartanNoAuthBaseTest {
 
     @Test
     public void sayHello(){
