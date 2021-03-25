72b0c43eac4baa0027129a1ba0a70fc441a31008
Akbar <akbar@cybertekschool.com>
2021-03-25 11:20:10 -0400

    test lifecycle annotations in JUnit5 completed

--- src/test/java/day1/TestLifecycleAnnotations.java
+++ src/test/java/day1/TestLifecycleAnnotations.java
@@ -8,6 +8,25 @@ import org.junit.jupiter.api.*;
 @DisplayName("Learning Test Lifecycle Annotations")
 public class TestLifecycleAnnotations {
 
+    @BeforeAll
+    public static void init(){
+        System.out.println("Before all is running");
+    }
+    @AfterAll
+    public static void close(){
+        System.out.println("After all is running");
+    }
+
+    @BeforeEach
+    public void initEach(){
+        System.out.println("Before Each is running");
+    }
+
+    @AfterEach
+    public void tearDownEach(){
+        System.out.println("After Each is running");
+    }
+
 
     @Test
     public void test1(){
