473379e2d7eb56c2715724200dcaf6882b580ac6
Akbar <akbar@cybertekschool.com>
2021-03-25 10:39:58 -0400

    added second test for testing name start with A

--- src/test/java/day1/Junit5_intro.java
+++ src/test/java/day1/Junit5_intro.java
@@ -10,8 +10,17 @@ public class Junit5_intro {
     public void test(){
         System.out.println("Learning JUnit5");
 
-        assertEquals(1,2) ;
+        assertEquals(1,1) ;
+        assertEquals(1,2,"Something is wrong !!");
 
     }
 
+    // add one more test ,
+    // to assert your name first character start with letter A
+    @Test
+    public void testName(){
+        String name = "Adnan";
+        assertEquals('A' , name.charAt(0) );
+//        assertTrue(name.startsWith("A"));
+    }
 }
