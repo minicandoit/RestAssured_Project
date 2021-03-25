beb5735a1b78e6f36dc86870a9a9587041101db2
Akbar <akbar@cybertekschool.com>
2021-03-25 10:29:13 -0400

    Adding static import for Assertions class with all assert methods and writing a failing test

--- src/test/java/day1/Junit5_intro.java
+++ src/test/java/day1/Junit5_intro.java
@@ -2,11 +2,16 @@ package day1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class Junit5_intro {
 
     @Test
     public void test(){
         System.out.println("Learning JUnit5");
+
+        assertEquals(1,2) ;
+
     }
 
 }
