dde4058b8f67db78ed80dab3797a80b944f64970
Akbar <akbar@cybertekschool.com>
2021-03-25 12:26:06 -0400

    nesting the matchers to make the intent absolutely clear

--- src/test/java/day1/HamcrestMatchersIntro.java
+++ src/test/java/day1/HamcrestMatchersIntro.java
@@ -17,6 +17,17 @@ public class HamcrestMatchersIntro {
         // assertThat( some actual value , some matcher that describe what you are trying to do)
         assertThat(5+5, is(10) ) ;
         assertThat(5+5, equalTo(10) );
+        // All matchers has 2 overloaded version
+        // first that accept actual value
+        // second that accept another matchers
+        // below examples is method is accepting another matchers equalTo to make it readable
+        assertThat(5+5 , is( equalTo(10) ) );
+
+        // negative assertion  5+5 is not 11
+        assertThat(5+5, not(11) );
+        assertThat(5+5, is( not(11) )   );
+        assertThat(5+5, is( not(  equalTo(11)  ) )    );
+
 
 
     }
