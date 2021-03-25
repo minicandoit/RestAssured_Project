5c3cc97abd6ae89797678c1f5bc215907f0767dc
Akbar <akbar@cybertekschool.com>
2021-03-25 14:23:31 -0400

    RestAssured static imports added

---
+++ src/test/java/day1/RestAssured_Intro.java
@@ -0,0 +1,11 @@
+package day1;
+
+import static io.restassured.RestAssured.* ;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.* ;
+
+public class RestAssured_Intro {
+
+
+
+}
