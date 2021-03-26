aaaec19cb92daa9694239ce1b7473e563019b600
Akbar <akbar@cybertekschool.com>
2021-03-26 10:23:33 -0400

    Day 2 : Path variable query params starter

---
+++ src/test/java/day2/SpartanTest_PathVariableQueryParam.java
@@ -0,0 +1,19 @@
+package day2;
+
+import test_util.SpartanNoAuthBaseTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import test_util.SpartanNoAuthBaseTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+
+public class SpartanTest_PathVariableQueryParam extends SpartanNoAuthBaseTest {
+
+    
+
+
+
+}
