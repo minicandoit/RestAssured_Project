ced451f9ff2ab9cdfe83db97230a814408a20de2
Akbar <akbar@cybertekschool.com>
2021-03-26 10:57:37 -0400

    Day 2 : rise of method chaining , 2 styles of providing path variables

--- src/test/java/day2/SpartanTest_PathVariableQueryParam.java
+++ src/test/java/day2/SpartanTest_PathVariableQueryParam.java
@@ -10,9 +10,36 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
 
+@DisplayName("Spartan Test with path variable and quert param")
 public class SpartanTest_PathVariableQueryParam extends SpartanNoAuthBaseTest {
 
-    
+    @Test
+    public void getOneSpartan(){
+
+        //get("/spartans/16").prettyPeek();
+
+        // I want to provide 16 as path variable|parameter
+        // I want to provide accept header
+
+        Response r1 =
+                given()
+                        .header("Accept", "application/json")
+                        .pathParam("spartan_id",16).
+                when()
+                        .get("/spartans/{spartan_id}")
+                        .prettyPeek()
+                ;
+
+        Response r2 =
+                given()
+                        // this is same as .header("Accept", "application/json")
+                        .accept("application/json").
+                when()
+                        // This is alternative way of providing
+                        // path variable and value directly in get method
+                        .get("/spartans/{spartan_id}" , 16 )
+                        .prettyPeek() ;
+    }
 
 
 
