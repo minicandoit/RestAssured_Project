0315b1ffb49ed8741f90581f077d60a55e45ee13
Akbar <akbar@cybertekschool.com>
2021-03-29 14:07:39 -0400

    Day 3 : POST Request starter

---
+++ src/test/java/day3/SpartanPostingData_Test.java
@@ -0,0 +1,32 @@
+package day3;
+import io.restassured.path.json.JsonPath;
+import test_util.SpartanNoAuthBaseTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import test_util.SpartanNoAuthBaseTest;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+
+@DisplayName("Testing adding data to Spartan app multiple way")
+public class SpartanPostingData_Test extends SpartanJsonPath_Test{
+
+    @DisplayName("POST /spartans with String")
+    @Test
+    public void testPostDataWithStringBody(){
+
+
+
+
+    }
+
+
+
+
+
+}
