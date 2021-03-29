0639dd8bb3e893359c79c3ac8ebd669a0e00e589
Akbar <akbar@cybertekschool.com>
2021-03-29 16:33:25 -0400

    Day 3 : Post request negative tests

---
+++ src/test/java/day3/SpartanPost_NegativeTest.java
@@ -0,0 +1,41 @@
+package day3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import test_util.SpartanNoAuthBaseTest;
+
+public class SpartanPost_NegativeTest extends SpartanNoAuthBaseTest {
+
+
+    @DisplayName("Post request without content type expect 415")
+    @Test
+    public void test1(){
+
+    }
+
+    @DisplayName("Post request without valid json expect 400")
+    @Test
+    public void test2(){
+
+    }
+
+    @DisplayName("Post request with valid json , bad name - expect 400 with detailed name error message")
+    @Test
+    public void test3(){
+
+    }
+
+    @DisplayName("Post request with bad name, phone - expect 400 with detailed name, phone error message")
+    @Test
+    public void test4(){
+
+    }
+
+
+    @DisplayName("Post request with bad name, phone , gender - expect 400 with 3 errors")
+    @Test
+    public void test5(){
+
+    }
+
+}
