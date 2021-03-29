02bd8d578591721e68fa6bae9b98b5393b20bf30
Akbar <akbar@cybertekschool.com>
2021-03-29 10:36:46 -0400

    Day 3 : JSONPATH  first example , 3 meaning of the word

--- src/test/java/day3/SpartanJsonPath_Test.java
+++ src/test/java/day3/SpartanJsonPath_Test.java
@@ -1,5 +1,6 @@
 package day3;
 
+import io.restassured.path.json.JsonPath;
 import test_util.SpartanNoAuthBaseTest;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -10,7 +11,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
 
-public class SpartanJsonPath_Test extends SpartanNoAuthBaseTest {
+public class SpartanJsonPath_Test extends SpartanNoAuthBaseTest{
 
     //http://YourKitchenIp:8000/api/spartans/160
 
@@ -19,10 +20,31 @@ public class SpartanJsonPath_Test extends SpartanNoAuthBaseTest {
 
         Response response =
                 given()
+                        .log().all()
                         .pathParam("id","160").
                 when()
                         .get("/spartans/{id}")
                         .prettyPeek() ;
+
+        // using path method to extract data
+        int myId = response.path("id") ;
+        System.out.println("myId = " + myId);
+
+        // Few meaning of JsonPath :
+            //1. just like xpath -- it is used to provide location of certain data
+            //2. JsonPath as a class coming from RestAssured to provide reusable methods to extract data
+            //3. jsonPath() method of Response object to get JsonPath object
+        JsonPath jp = response.jsonPath() ;  // number 2 for variable type , number 3 for method
+
+        myId =   jp.getInt("id") ;
+        System.out.println(" the result is = " + myId);
+
+
+
+
+
+
+
 
 
     }
