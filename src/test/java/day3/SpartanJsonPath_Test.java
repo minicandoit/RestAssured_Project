0569b5aeac4713762a81ae458884d590e1b00a2c
Akbar <akbar@cybertekschool.com>
2021-03-29 11:30:06 -0400

    Day 3 : JSONPATH  : get data in json array

--- src/test/java/day3/SpartanJsonPath_Test.java
+++ src/test/java/day3/SpartanJsonPath_Test.java
@@ -7,6 +7,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
 import test_util.SpartanNoAuthBaseTest;
 
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static io.restassured.RestAssured.* ;
 import static org.hamcrest.Matchers.* ;
@@ -42,11 +44,43 @@ public class SpartanJsonPath_Test extends SpartanNoAuthBaseTest{
         long phoneNum =  jp.getLong("phone") ;
         System.out.println("phoneNum = " + phoneNum);
 
+        String myName = jp.getString("name") ;
+        System.out.println("myName = " + myName);
+
+
         System.out.println("Save whole json object into map = "
                 + jp.getMap(""));
 
+        Map<String, Object> resultJsonInMap = jp.getMap("") ;
+        System.out.println("resultJsonInMap = " + resultJsonInMap);
+
 
     }
+
+    @DisplayName("Extract data from GET /spartans ")
+    @Test
+    public void testGetAllSpartans(){
+
+//        Response response = get("/spartans");
+//        JsonPath jp = response.jsonPath() ;
+
+        JsonPath jp = get("/spartans").jsonPath() ;
+
+        // print first id in the json array response
+       // [{},{},{} ]
+        System.out.println("jp.getInt(\"id[0]\") = "
+                + jp.getInt("id[0]"));
+        // print second json object name in the json array response
+        System.out.println("jp.getString(\"name[1]\") = "
+                + jp.getString("name[1]"));
+
+
+    }
+
+
+
+
+
 
 
 }
