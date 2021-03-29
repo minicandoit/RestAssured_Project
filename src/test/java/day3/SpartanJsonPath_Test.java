aabe53edf69615c873ff08bdfdfba044b28968a2
Akbar <akbar@cybertekschool.com>
2021-03-29 12:21:26 -0400

    Day 3 : JSONPATH  : get data in json array as List starter

--- src/test/java/day3/SpartanJsonPath_Test.java
+++ src/test/java/day3/SpartanJsonPath_Test.java
@@ -118,6 +118,23 @@ public class SpartanJsonPath_Test extends SpartanNoAuthBaseTest{
     }
 
 
+    @DisplayName("Saving json array fields into List")
+    @Test
+    public void testSavingJsonArrayFieldsIntoList(){
+
+        JsonPath jp =
+                given()
+                        .queryParam("nameContains","Abigale")
+                        .queryParam("gender","Male")
+                        .log().all().
+                        when()
+                        .get("/spartans/search")
+                        .prettyPeek()
+                        .jsonPath();
+
+    }
+
+
 
 
 
