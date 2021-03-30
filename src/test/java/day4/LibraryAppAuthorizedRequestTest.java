8a7a6de06270c8f5693bdbbaafdd2440d20ad72a
Akbar <akbar@cybertekschool.com>
2021-03-30 15:07:49 -0400

    Day 4 : LibraryApp Test starter

---
+++ src/test/java/day4/LibraryAppAuthorizedRequestTest.java
@@ -0,0 +1,18 @@
+package day4;
+
+import test_util.LibraryAppBaseTest;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class LibraryAppAuthorizedRequestTest extends LibraryAppBaseTest {
+
+
+
+}
