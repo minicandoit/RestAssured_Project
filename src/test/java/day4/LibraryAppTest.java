8e9af64960f0e0ca8909e7c37e1be82e81b21e30
Akbar <akbar@cybertekschool.com>
2021-03-30 14:13:01 -0400

    Day 4 : GET /Dashboard_stats starter

--- src/test/java/day4/LibraryAppTest.java
+++ src/test/java/day4/LibraryAppTest.java
@@ -1,23 +1,13 @@
 package day4;
 
 import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
-import pojo.Spartan;
-import spartan_util.SpartanUtil;
-import test_util.SpartanNoAuthBaseTest;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.*;
-import test_util.SpartanNoAuthBaseTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static io.restassured.RestAssured.* ;
-import static org.hamcrest.Matchers.* ;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 @DisplayName("Library App Simple Test")
 public class LibraryAppTest {
@@ -90,6 +80,17 @@ public class LibraryAppTest {
                 .statusCode(200)
                 .body("email" , is( username) )
                 ;
+
+
+    }
+
+
+    @DisplayName("Test GET /dashboard_stats endpoint")
+    @Test
+    public void testDashboardNumbers(){
+
+
+
 
 
     }
