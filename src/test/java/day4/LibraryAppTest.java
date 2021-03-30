795f9b0a5ee1aa94f327695fc89e5f2e269384d0
Akbar <akbar@cybertekschool.com>
2021-03-30 12:27:11 -0400

    Day 4 : Library app test starter

---
+++ src/test/java/day4/LibraryAppTest.java
@@ -0,0 +1,44 @@
+package day4;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import pojo.Spartan;
+import spartan_util.SpartanUtil;
+import test_util.SpartanNoAuthBaseTest;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+import test_util.SpartanNoAuthBaseTest;
+
+import java.io.File;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.* ;
+import static org.hamcrest.Matchers.* ;
+
+@DisplayName("Library App Simple Test")
+public class LibraryAppTest {
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
