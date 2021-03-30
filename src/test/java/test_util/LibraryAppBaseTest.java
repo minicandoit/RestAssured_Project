1185d48b12d8383427cd33c87f05e15815b07db9
Akbar <akbar@cybertekschool.com>
2021-03-30 15:07:16 -0400

    Day 4 : LibraryAppBaseTest initial , with getToken static method

---
+++ src/test/java/test_util/LibraryAppBaseTest.java
@@ -0,0 +1,39 @@
+package test_util;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.*;
+
+public class LibraryAppBaseTest {
+
+    @BeforeAll
+    public static void init(){
+
+        baseURI  = "http://library1.cybertekschool.com" ;
+        basePath = "/rest/v1" ;
+
+    }
+
+    public static String getToken(String username, String password){
+
+        return given()
+                .contentType(ContentType.URLENC)
+                .formParam("email", username)
+                .formParam("password" , password).
+                when()
+                .post("/login")
+                .path("token");
+
+    }
+
+
+    @AfterAll
+    public static void cleanup(){
+        reset();
+    }
+
+
+
+}
