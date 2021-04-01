0ac4abc7eb0185761b6c2a7fdb603408ffc8894a
Akbar <akbar@cybertekschool.com>
2021-04-01 14:47:01 -0400

    Day 5 : CSVSource for providing multiple data per iteration of ParameterizedTest in Zipcode API

--- src/test/java/day5/CsvSourceParameterizedTest.java
+++ src/test/java/day5/CsvSourceParameterizedTest.java
@@ -3,6 +3,7 @@ package day5;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -15,8 +16,7 @@ public class CsvSourceParameterizedTest {
 //            10, 9 , 19
 
     @ParameterizedTest
-    @CsvSource({
-                "1, 3 , 4",
+    @CsvSource({"1, 3 , 4",
                 "2, 3 , 5",
                 "8, 7 , 150",
                 "10, 9 , 19"
@@ -30,7 +30,38 @@ public class CsvSourceParameterizedTest {
 
     }
 
+    // Write a parameterized test for this request
+    // GET https://api.zippopotam.us/us/{state}/{city}
+    @ParameterizedTest
+    @CsvSource({
+        "NY, New York",
+        "CO, Denver",
+        "VA, Fairfax",
+        "VA, Arlington",
+        "MA, Boston",
+        "NY, New York",
+        "MD, Annapolis"
+    })
+    public void testStateCityToZipEndpoint(String stateArg , String cityArg){
 
+        System.out.println("stateArg = " + stateArg);
+        System.out.println("cityArg = " + cityArg);
+
+        given()
+                .baseUri("https://api.zippopotam.us")
+                .pathParam("state",  stateArg)
+                .pathParam("city",  cityArg)
+                .log().uri().
+        when()
+                .get("/us/{state}/{city}").
+        then()
+                .log().body()
+                .statusCode(200)
+                ;
+        // try printing out the zip code count
+
+
+    }
 
 
 
