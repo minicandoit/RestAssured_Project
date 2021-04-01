1b617b273fe36ded8941078f58beef2238944dbb
Akbar <akbar@cybertekschool.com>
2021-04-01 12:56:37 -0400

    Day 5 : JUnit 5 Parameterized Test ValueSource for zip code api data driven test

--- src/test/java/day5/ParametrizedTestInJunit5.java
+++ src/test/java/day5/ParametrizedTestInJunit5.java
@@ -2,6 +2,8 @@ package day5;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ParametrizedTestInJunit5 {
@@ -28,15 +30,27 @@ public class ParametrizedTestInJunit5 {
 
     }
 
-    // SEND GET REQUEST TO http://api.zippopotam.us/us/{zipcode}
+    // SEND GET REQUEST TO https://api.zippopotam.us/us/{zipcode}
     // with these zipcodes 22030,22031, 22032, 22033 , 22034, 22035, 22036
     // check status code 200
 
     @ParameterizedTest
     @ValueSource(shorts = { 22030,22031, 22032, 22033 , 22034, 22035, 22036})
-    public void testZipcodes( short zipcode  ){
+    public void testZipcodes( short zip  ){
 
-        System.out.println("zipcode = " + zipcode);
+        System.out.println("zipcode = " + zip);
+
+        given()
+                .baseUri("https://api.zippopotam.us")
+                .log().all()
+                .pathParam("zipcode" ,   zip  ).
+        when()
+                .get("/us/{zipcode}").
+        then()
+                .statusCode( 200 )
+                .log().all()
+                ;
+
 
 
     }
