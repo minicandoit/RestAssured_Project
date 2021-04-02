59d98c16da07bea2707aaefd98038ebb7118ec66
Akbar <akbar@cybertekschool.com>
2021-04-02 11:49:34 -0400

    Day 6 : HR_ORDS_API_ parameterized test for getting single country with method source

--- src/test/java/day6/HR_ORDS_ParametrizedTest.java
+++ src/test/java/day6/HR_ORDS_ParametrizedTest.java
@@ -69,6 +69,17 @@ public class HR_ORDS_ParametrizedTest extends HR_ORDS_API_BaseTest {
    public void testCountryWithMethodSource(String countryIdArg){
 
        System.out.println("countryIdArg = " + countryIdArg);
+       // GET /countries/{country_id}
+       given()
+               .log().uri()
+                .pathParam("country_id", countryIdArg).
+       when()
+               .get("/countries/{country_id}").
+       then()
+               .log().body()
+               .statusCode(200)
+               .body("count", is(1) )
+       ;
 
    }
 
@@ -77,7 +88,12 @@ public class HR_ORDS_ParametrizedTest extends HR_ORDS_API_BaseTest {
    // write static method that return list of country ids
     public static List<String> getManyCountryIds(){
 
-        List<String> countryNameList = Arrays.asList("AR", "BE","US") ;
+        //List<String> countryNameList = Arrays.asList("AR", "BE","US") ;
+        // SEND REQUEST TO GET /countries and save the country_id as List<String>
+
+        List<String> countryNameList = get("/countries")
+                                    .jsonPath().getList("items.country_id", String.class) ;
+
         return countryNameList ;
 
     }
