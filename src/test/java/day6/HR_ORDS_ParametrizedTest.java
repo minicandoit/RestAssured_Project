8e7818dd1ed0583549996a4718ddd0d2859dd74a
Akbar <akbar@cybertekschool.com>
2021-04-02 11:11:19 -0400

    Day 6 : HR_ORDS_API_ parameterized test starter

---
+++ src/test/java/day6/HR_ORDS_ParametrizedTest.java
@@ -0,0 +1,13 @@
+package day6;
+
+import test_util.HR_ORDS_API_BaseTest;
+
+public class HR_ORDS_ParametrizedTest extends HR_ORDS_API_BaseTest {
+
+    //GET http://18.235.32.166:1000/ords/hr/api/regions
+    // base_uri = http://18.235.32.166:1000
+    // base_path =  /ords/hr/api
+    // resources /regions
+
+
+}
