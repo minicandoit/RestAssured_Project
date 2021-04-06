165b5bb4108d40b962c0f1e2e42800b6f94cf30a
Akbar <akbar@cybertekschool.com>
2021-04-06 14:12:40 -0400

    Day 7 : Employee POJO with only 5 fields

---
+++ src/test/java/pojo/Employees.java
@@ -0,0 +1,17 @@
+package pojo;
+
+
+import lombok.*;
+
+// we just want to represent the Employee data with these fields and ignore any other fields
+@Getter @Setter
+@ToString
+public class Employees {
+
+    private int employee_id;
+    private String first_name;
+    private String last_name;
+    private int salary;
+    private int department_id;
+
+}
