1ea27656645a7cc49ab9469d5da035266604c8ce
Akbar <akbar@cybertekschool.com>
2021-04-06 12:01:09 -0400

    Day 7 : HR Ords api /regions and save to List of pojo starter

--- src/test/java/day7/HR_ORDS_API_DeserializationTest.java
+++ src/test/java/day7/HR_ORDS_API_DeserializationTest.java
@@ -1,6 +1,12 @@
 package day7;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pojo.Region;
 import test_util.HR_ORDS_API_BaseTest;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -10,7 +16,19 @@ public class HR_ORDS_API_DeserializationTest extends HR_ORDS_API_BaseTest {
 
     // Send request to /regions and save the result into List<Region>
     // assert the list has size 4
+    @DisplayName("GET /regions")
+    @Test
+    public void testGetAllRegionAndSaveToListOfPOJO(){
 
+        // saving json array into List<Region>
+        List<Region> allRegions =
+                        get("/regions")
+                        .jsonPath()
+                        .getList("items" , Region.class) ;
+
+        System.out.println("allRegions = " + allRegions);
+
+    }
 
 
 }
