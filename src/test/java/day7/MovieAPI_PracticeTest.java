d5f75236bae266fdea87f634092751fff7cc5576
Akbar <akbar@cybertekschool.com>
2021-04-06 15:12:00 -0400

    Day 7 : Movie API POJO Practice stater

---
+++ src/test/java/day7/MovieAPI_PracticeTest.java
@@ -0,0 +1,23 @@
+package day7;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MovieAPI_PracticeTest {
+
+
+    // save the result of your request
+    // SEND GET http://www.omdbapi.com/?t=Avenger&apikey=YOUR OWN API KEY goes here
+
+    // save the response into Movie POJO , title Str, year int , Released str ,Language
+    // ignore any unknown properties
+    // match the json fields to pojo fields
+    @DisplayName("GET GET http://www.omdbapi.com/?t=Avenger&apikey=YOUR OWN API KEY")
+    @Test
+    public void testMovieToPOJO(){
+
+
+
+    }
+
+}
