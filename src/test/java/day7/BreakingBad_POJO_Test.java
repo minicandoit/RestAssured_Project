d471fb0470a7b2ae4b607dfce7c757e23d6299ef
Akbar <akbar@cybertekschool.com>
2021-04-06 16:43:16 -0400

    Day 7 : Breaking bad character pojo practice completed task 2, 3

--- src/test/java/day7/BreakingBad_POJO_Test.java
+++ src/test/java/day7/BreakingBad_POJO_Test.java
@@ -2,7 +2,9 @@ package day7;
 
 import static io.restassured.RestAssured.*;
 import org.junit.jupiter.api.*;
-import pojo.Character ;
+import pojo.Character;
+
+import java.util.List;
 
 public class BreakingBad_POJO_Test {
 
@@ -28,8 +30,24 @@ public class BreakingBad_POJO_Test {
         System.out.println("c1 = " + c1);
 
         // save all items into List<Character>
-        // print all character name who appeared exactly 5 times  --- check appearance list size
+        List<Character> allCharacters = get("/characters").jsonPath()
+                                        .getList("", Character.class) ;
+        //allCharacters.forEach( each -> System.out.println(each ) ) ;
 
+        // print all character name who appeared exactly 5 times (all 5 seasons) --- check appearance list size
+        for ( Character each : allCharacters){
+            if( each.getAppearance().size()== 5 ){
+                System.out.println(each.getName() );
+            }
+        }
+        // find out all characters showed up only in season 3
+        for ( Character each : allCharacters){
+
+            if( each.getAppearance().size()==1 && each.getAppearance().get(0)==3){
+                System.out.println("each = " + each);
+            }
+
+        }
 
 
     }
