9d48f91d3e7414a0dfb5abbeabc65f86f7ba325f
Akbar <akbar@cybertekschool.com>
2021-04-06 16:27:16 -0400

    Day 7 : Breaking bad character pojo practice completed task 1

--- src/test/java/day7/BreakingBad_POJO_Test.java
+++ src/test/java/day7/BreakingBad_POJO_Test.java
@@ -2,6 +2,7 @@ package day7;
 
 import static io.restassured.RestAssured.*;
 import org.junit.jupiter.api.*;
+import pojo.Character ;
 
 public class BreakingBad_POJO_Test {
 
@@ -22,8 +23,14 @@ public class BreakingBad_POJO_Test {
 
         // Send request to GET https://www.breakingbadapi.com/api/characters
         // save first item into Character pojo
+        Character c1 = get("/characters").jsonPath()
+                        .getObject("[0]", Character.class) ;
+        System.out.println("c1 = " + c1);
+
         // save all items into List<Character>
         // print all character name who appeared exactly 5 times  --- check appearance list size
+
+
 
     }
 
