aafb12d77d52af2bb015af60ecc8ab6eed3dc6bb
Akbar <akbar@cybertekschool.com>
2021-04-06 10:28:53 -0400

    Day 7 : De-Serialization starter

---
+++ src/test/java/day7/SpartanDeserialization_Test.java
@@ -0,0 +1,25 @@
+package day7;
+
+import org.junit.jupiter.api.DisplayName;
+import test_util.SpartanNoAuthBaseTest;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import io.restassured.response.Response;
+
+public class SpartanDeserialization_Test extends SpartanNoAuthBaseTest {
+
+    // Serialization    : Java Object to Json (or any other type of text)
+    // De-Serialization : Json(text) to Java
+
+    @DisplayName("GET /spartans/{id}")
+    public void testGetOneData(){
+
+
+
+
+    }
+
+
+}
