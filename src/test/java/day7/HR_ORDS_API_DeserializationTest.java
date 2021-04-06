c7156113e5aa22c105fdbb3af7614b9cd0b49314
Akbar <akbar@cybertekschool.com>
2021-04-06 11:50:46 -0400

    Day 7 : HR Ords api /regions and save to List of pojo starter

---
+++ src/test/java/day7/HR_ORDS_API_DeserializationTest.java
@@ -0,0 +1,16 @@
+package day7;
+
+import test_util.HR_ORDS_API_BaseTest;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HR_ORDS_API_DeserializationTest extends HR_ORDS_API_BaseTest {
+
+
+    // Send request to /regions and save the result into List<Region>
+    // assert the list has size 4
+
+
+
+}
