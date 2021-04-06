8b9a082edf629c2ab1e42a8958571048aa41bd65
Akbar <akbar@cybertekschool.com>
2021-04-06 10:51:21 -0400

    Day 7 : Json to Java using response.as method if no json path needed to get to json Object

--- src/test/java/day7/SpartanDeserialization_Test.java
+++ src/test/java/day7/SpartanDeserialization_Test.java
@@ -2,6 +2,7 @@ package day7;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pojo.SpartanPOJO;
 import test_util.SpartanNoAuthBaseTest;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -18,7 +19,23 @@ public class SpartanDeserialization_Test extends SpartanNoAuthBaseTest {
     @Test
     public void testGetOneData(){
 
+        given()
+                .pathParam("id", 8001).
+        when()
+                .get("/spartans/{id}").
+        then()
+                .statusCode(200)
+                .log().body() ;
 
+    // Send same request , store the result into SpartanPOJO object
+
+        Response response = given()
+                            .pathParam("id", 8001).
+                            when()
+                            .get("/spartans/{id}") ;
+
+        SpartanPOJO sp = response.as(SpartanPOJO.class) ;
+        System.out.println("sp = " + sp);
 
 
     }
