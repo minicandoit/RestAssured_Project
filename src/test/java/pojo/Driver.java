7906c93cb182ca9997cb92626d0714e6dec1154b
Akbar <akbar@cybertekschool.com>
2021-04-08 10:30:53 -0400

    Day 8 : Formula One API driver POJO

---
+++ src/test/java/pojo/Driver.java
@@ -0,0 +1,18 @@
+package pojo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+
+public class Driver {
+
+    private String driverId;
+    private String url;
+    private String givenName;
+    private String familyName;
+    private String dateOfBirth;
+    private String nationality;
+
+}
