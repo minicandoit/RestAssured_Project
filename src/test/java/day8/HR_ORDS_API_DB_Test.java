8072956e44adba9a646a004883ee5731eef4d894
Akbar <akbar@cybertekschool.com>
2021-04-08 15:19:59 -0400

    Day 8 : HR ORDS initial DB connection test

--- src/test/java/day8/HR_ORDS_API_DB_Test.java
+++ src/test/java/day8/HR_ORDS_API_DB_Test.java
@@ -1,5 +1,7 @@
 package day8;
 
+import org.junit.jupiter.api.Test;
+import test_util.DB_Utility;
 import test_util.HR_ORDS_API_BaseTest;
 
 import java.util.List;
@@ -8,6 +10,16 @@ import static io.restassured.RestAssured.*;
 
 public class HR_ORDS_API_DB_Test extends HR_ORDS_API_BaseTest {
 
+    // We added DB Connection and DB Destroy method into base test
+    // We added the base uri of HR ORDS API into configuration file
+
+    @Test
+    public void testHR_ORDS_API(){
+
+        DB_Utility.runQuery("SELECT * FROM REGIONS") ;
+        DB_Utility.displayAllData();
+
+    }
 
 
 }
