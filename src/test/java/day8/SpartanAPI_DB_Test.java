e4a4a590952f4f1aa2940c15f9d2f3a26b0cd137
Akbar <akbar@cybertekschool.com>
2021-04-08 12:40:06 -0400

    Day 8 : API DB Validation starter

--- src/test/java/day8/SpartanAPI_DB_Test.java
+++ src/test/java/day8/SpartanAPI_DB_Test.java
@@ -1,5 +1,6 @@
 package day8;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import test_util.ConfigurationReader;
 import test_util.DB_Utility;
@@ -10,18 +11,31 @@ public class SpartanAPI_DB_Test extends SpartanNoAuthBaseTest {
 
     @Test
     public void testDB_Connection(){
+//        // this will create HR connection according to your properties file
+////        DB_Utility.createConnection();
+//        String url = ConfigurationReader.getProperty("spartan.database.url");
+//        String username = ConfigurationReader.getProperty("spartan.database.username");
+//        String password = ConfigurationReader.getProperty("spartan.database.password");
+//        DB_Utility.createConnection(url,username,password);
+         DB_Utility.runQuery("SELECT * FROM SPARTANS") ;
+         DB_Utility.displayAllData();
 
-        // this will create HR connection according to your properties file
-//        DB_Utility.createConnection();
-        String url = ConfigurationReader.getProperty("spartan.database.url");
-        String username = ConfigurationReader.getProperty("spartan.database.username");
-        String password = ConfigurationReader.getProperty("spartan.database.password");
-        DB_Utility.createConnection(url,username,password);
+//        DB_Utility.destroy();
+    }
 
+    @DisplayName("Test GET /spartans/{id} match DB Data")
+    @Test
+    public void testGetSingleSpartanResponseMatchDB_Result(){
 
-        DB_Utility.destroy();
+        // expected data -- DB query result
+        // actual   data -- api response json
+
+        // we want to test spartan with ID of 8003 , api response json match database data
+        // RUN QUERY  SELECT * FROM SPARTANS WHERE SPARTAN_ID = 8003
+
 
 
     }
+
 
 }
