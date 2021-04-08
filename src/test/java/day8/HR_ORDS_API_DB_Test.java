706126f13e1daae98468d121dc930fbb951eefed
Akbar <akbar@cybertekschool.com>
2021-04-08 15:11:15 -0400

    Day 8 : HR_ORDS api db combo starter

---
+++ src/test/java/day8/HR_ORDS_API_DB_Test.java
@@ -0,0 +1,13 @@
+package day8;
+
+import test_util.HR_ORDS_API_BaseTest;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+
+public class HR_ORDS_API_DB_Test extends HR_ORDS_API_BaseTest {
+
+
+
+}
