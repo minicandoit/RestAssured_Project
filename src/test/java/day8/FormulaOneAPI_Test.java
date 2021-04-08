e64edf34d5a5bbff1dcd32a97ac42af7afe4c80d
Akbar <akbar@cybertekschool.com>
2021-04-08 10:39:56 -0400

    Day 8 : Formula One API driver POJO task 1 complete

--- src/test/java/day8/FormulaOneAPI_Test.java
+++ src/test/java/day8/FormulaOneAPI_Test.java
@@ -2,10 +2,12 @@ package day8;
 
 import static io.restassured.RestAssured.*;
 
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import pojo.Driver;
 
 public class FormulaOneAPI_Test {
 
@@ -20,6 +22,13 @@ public class FormulaOneAPI_Test {
     @DisplayName("GET /drivers.json and save result to pojo")
     @Test
     public void testDrivers(){
+
+        // Get first Driver as Driver POJO
+        JsonPath jp = get("/drivers.json").jsonPath() ;
+        Driver d1 = jp.getObject("MRData.DriverTable.Drivers[0]", Driver.class) ;
+        System.out.println("d1 = " + d1);
+
+        // Get all drivers as List<Driver>
 
 
 
