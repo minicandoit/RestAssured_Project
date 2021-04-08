2db3adb7fa6acecf36c564e8add397feef8c0614
Akbar <akbar@cybertekschool.com>
2021-04-08 12:28:22 -0400

    Day 8 : API_DB_Starter initial connection test

--- src/test/java/day8/SpartanAPI_DB_Test.java
+++ src/test/java/day8/SpartanAPI_DB_Test.java
@@ -1,6 +1,8 @@
 package day8;
 
 import org.junit.jupiter.api.Test;
+import test_util.ConfigurationReader;
+import test_util.DB_Utility;
 import test_util.SpartanNoAuthBaseTest;
 
 public class SpartanAPI_DB_Test extends SpartanNoAuthBaseTest {
@@ -9,7 +11,15 @@ public class SpartanAPI_DB_Test extends SpartanNoAuthBaseTest {
     @Test
     public void testDB_Connection(){
 
+        // this will create HR connection according to your properties file
+//        DB_Utility.createConnection();
+        String url = ConfigurationReader.getProperty("spartan.database.url");
+        String username = ConfigurationReader.getProperty("spartan.database.username");
+        String password = ConfigurationReader.getProperty("spartan.database.password");
+        DB_Utility.createConnection(url,username,password);
 
+
+        DB_Utility.destroy();
 
 
     }
