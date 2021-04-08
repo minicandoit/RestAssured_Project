e7009465231d9efffcf701141a54ac8b0e16845e
Akbar <akbar@cybertekschool.com>
2021-04-08 12:24:00 -0400

    Day 8 : API_DB_Starter

---
+++ src/test/java/day8/SpartanAPI_DB_Test.java
@@ -0,0 +1,17 @@
+package day8;
+
+import org.junit.jupiter.api.Test;
+import test_util.SpartanNoAuthBaseTest;
+
+public class SpartanAPI_DB_Test extends SpartanNoAuthBaseTest {
+
+
+    @Test
+    public void testDB_Connection(){
+
+
+
+
+    }
+
+}
