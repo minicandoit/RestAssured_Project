0fbde44bb1211c518ab65114f4677f5d967aca2b
Akbar <akbar@cybertekschool.com>
2021-04-08 15:16:45 -0400

    Day 8 : Added HR BASE URI into configuration.properties

--- src/test/java/test_util/SpartanNoAuthBaseTest.java
+++ src/test/java/test_util/SpartanNoAuthBaseTest.java
@@ -11,7 +11,7 @@ public abstract class SpartanNoAuthBaseTest {
         // as a homework , put these information
         // in configurations.properties file
         // this will set the part of URL at RestAssured
-        RestAssured.baseURI     = "http://18.235.32.166:8000"  ;
+        RestAssured.baseURI     = ConfigurationReader.getProperty("hr.ords.baseuri")  ;
 //        RestAssured.port = 8000 ;
         RestAssured.basePath    = "/api" ;
 
