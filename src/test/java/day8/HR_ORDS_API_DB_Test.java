bc8349f6a5b55de756242261e4bd44f8ec1210aa
Akbar <akbar@cybertekschool.com>
2021-04-08 15:40:06 -0400

    Day 8 : Test First Region data from DB and API

--- src/test/java/day8/HR_ORDS_API_DB_Test.java
+++ src/test/java/day8/HR_ORDS_API_DB_Test.java
@@ -1,12 +1,16 @@
 package day8;
 
 import org.junit.jupiter.api.Test;
+import pojo.Region;
 import test_util.DB_Utility;
 import test_util.HR_ORDS_API_BaseTest;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static test_util.DB_Utility.*;
 
 public class HR_ORDS_API_DB_Test extends HR_ORDS_API_BaseTest {
 
@@ -16,8 +20,30 @@ public class HR_ORDS_API_DB_Test extends HR_ORDS_API_BaseTest {
     @Test
     public void testHR_ORDS_API(){
 
-        DB_Utility.runQuery("SELECT * FROM REGIONS") ;
-        DB_Utility.displayAllData();
+        runQuery("SELECT * FROM REGIONS WHERE REGION_ID = 1") ;
+        displayAllData();
+        // send request to GET /regions/{region_id} and compare the result with DB result
+
+        // save expected data for region_id of 1 into List
+        List<String> firstRowAsExpectedList = getRowDataAsList(1) ;
+        System.out.println("firstRowAsExpectedList = " + firstRowAsExpectedList);
+        // send API request to GET /regions/{region_id} with id of 1 , save the result into POJO
+        Region r1 = given()
+                        .pathParam("region_id",1).
+                     when()
+                         .get("/regions/{region_id}")
+                        .jsonPath()
+                        .getObject("items[0]", Region.class) ;
+        System.out.println("r1 = " + r1);
+
+        //get("/regions/{region_id}" , 1)
+        // compare the result
+
+
+
+
+
+
 
     }
 
