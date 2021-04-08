597f02c92300cb4e08a6b82587181d003064ed19
Akbar <akbar@cybertekschool.com>
2021-04-08 10:44:48 -0400

    Day 8 : Added ignore unknown properties to pojo

--- src/test/java/pojo/Driver.java
+++ src/test/java/pojo/Driver.java
@@ -1,11 +1,12 @@
 package pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Getter
 @Setter
 @ToString
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Driver {
 
     private String driverId;
