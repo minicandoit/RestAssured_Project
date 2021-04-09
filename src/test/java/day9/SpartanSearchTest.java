3062c8bbd50ce53ae4c0f975f281ecf818763a22
Akbar <akbar@cybertekschool.com>
2021-04-09 11:15:37 -0400

    Day 9 : Initial query run for search result

---
+++ src/test/java/day9/SpartanSearchTest.java
@@ -0,0 +1,28 @@
+package day9;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import test_util.DB_Utility;
+import test_util.SpartanNoAuthBaseTest;
+
+public class SpartanSearchTest extends SpartanNoAuthBaseTest {
+
+    @DisplayName("Test GET /spartans/search result with DB result")
+    @Test
+    public void testSearch(){
+
+        // Search for nameContains a and gender Female
+        // compare the count with DB result
+        String query = "SELECT * FROM SPARTANS WHERE LOWER(NAME) LIKE '%a%' and GENDER = 'Female'" ;
+        DB_Utility.runQuery(query) ;
+
+        DB_Utility.displayAllData();
+
+
+
+
+
+
+    }
+
+}
