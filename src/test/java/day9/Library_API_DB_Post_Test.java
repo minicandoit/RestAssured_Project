82d8474797fb8ad38c855f9242da2782d40c83c7
Akbar <akbar@cybertekschool.com>
2021-04-09 14:48:32 -0400

    Day 9 : Add book extract the id

--- src/test/java/day9/Library_API_DB_Post_Test.java
+++ src/test/java/day9/Library_API_DB_Post_Test.java
@@ -1,8 +1,13 @@
 package day9;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import test_util.LibraryAppBaseTest;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
 
 public class Library_API_DB_Post_Test extends LibraryAppBaseTest {
 
@@ -15,7 +20,22 @@ public class Library_API_DB_Post_Test extends LibraryAppBaseTest {
     @Test
     public void testAddBookPersisted(){
 
+        Map<String,Object> randomBookMapBody = getRandomBook() ;
+        System.out.println("randomBookMapBody = " + randomBookMapBody);
 
+        int newBookId =  given()
+                                .header("X-LIBRARY-TOKEN" , librarianToken)
+                                .contentType(ContentType.URLENC)
+                                .formParams(randomBookMapBody).
+                         when()
+                                .post("/add_book").
+                         then()
+                                // normally it return 201 , this one decided to return 200
+                                .statusCode(200)
+                                .log().body()
+                            .extract()
+                                .jsonPath().getInt("book_id") ;
+        System.out.println("newBookId = " + newBookId);
     }
 
 
