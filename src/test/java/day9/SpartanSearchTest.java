2deb40ad83fcf6712492e17290cf8202dcbd7740
Akbar <akbar@cybertekschool.com>
2021-04-09 11:41:32 -0400

    Day 9 : Parameterizing the Search with CSVSource

--- src/test/java/day9/SpartanSearchTest.java
+++ src/test/java/day9/SpartanSearchTest.java
@@ -2,6 +2,8 @@ package day9;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import test_util.DB_Utility;
 import test_util.SpartanNoAuthBaseTest;
 
@@ -32,12 +34,40 @@ public class SpartanSearchTest extends SpartanNoAuthBaseTest {
                 .body("totalElement" , equalTo( expectedCount ) )
                 // content jsonpath is pointing to json array , so we can directly check the size using hasSize matcher
                 .body("content" , hasSize( expectedCount)  )
-
         ;
 
+    }
 
+    @ParameterizedTest
+    @CsvSource({
+                    "e, Male",
+                    "le, Female",
+                    "k, Male",
+                    "g, Male",
+                    "u, Female",
+                    "f, Male"
+             })
+    public void testSearchParameterized(String nameArg, String genderArg){
 
+        String query = "SELECT * FROM SPARTANS WHERE LOWER(NAME) LIKE '%" + nameArg + "%' and GENDER = '"+ genderArg +"'" ;
+        System.out.println("query = " + query);
+        DB_Utility.runQuery(query) ;
+
+        int expectedCount = DB_Utility.getRowCount() ;
+        System.out.println("expectedCount = " + expectedCount);
+
+        given()
+                .queryParam("nameContains" , nameArg)
+                .queryParam("gender" , genderArg).
+        when()
+                .get("/spartans/search").
+        then()
+                .statusCode(200)
+                .body("totalElement" , equalTo(expectedCount))
+                ;
 
     }
+
+
 
 }
