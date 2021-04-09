2373905f02a0843c8a79e1c20e218603a47d2d0b
Akbar <akbar@cybertekschool.com>
2021-04-09 11:25:27 -0400

    Day 9 : send request and test the result against expected DB Count

--- src/test/java/day9/SpartanSearchTest.java
+++ src/test/java/day9/SpartanSearchTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import test_util.DB_Utility;
 import test_util.SpartanNoAuthBaseTest;
 
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class SpartanSearchTest extends SpartanNoAuthBaseTest {
 
     @DisplayName("Test GET /spartans/search result with DB result")
@@ -19,6 +22,16 @@ public class SpartanSearchTest extends SpartanNoAuthBaseTest {
         int expectedCount = DB_Utility.getRowCount()  ;
         System.out.println("count = " + expectedCount);
 
+        given()
+                .queryParam("nameContains", "a")
+                .queryParam("gender", "Female").
+        when()
+                .get("/spartans/search").
+        then()
+                .statusCode(200)
+                .body("totalElement" , equalTo( expectedCount ) )
+
+        ;
 
 
 
