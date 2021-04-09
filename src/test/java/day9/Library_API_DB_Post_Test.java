c2525815a1b9c7dc96010da6923ab014c027ad10
Akbar <akbar@cybertekschool.com>
2021-04-09 14:39:02 -0400

    Day 9 : Add book initial set up

---
+++ src/test/java/day9/Library_API_DB_Post_Test.java
@@ -0,0 +1,23 @@
+package day9;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import test_util.LibraryAppBaseTest;
+
+public class Library_API_DB_Post_Test extends LibraryAppBaseTest {
+
+
+    // Add a random book using POST /add_book
+    // grab the id  and write a query to book information with this id
+    // assert ALL data match exactly as it was posted.
+
+    @DisplayName("Add One book ,Validate from DB")
+    @Test
+    public void testAddBookPersisted(){
+
+
+    }
+
+
+
+}
