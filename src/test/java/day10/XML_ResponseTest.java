6df71898a50e200aff2259a4f1bdf1d076a1d717
Akbar <akbar@cybertekschool.com>
2021-04-12 10:23:37 -0400

    Day 10 : XML Response Starter

---
+++ src/test/java/day10/XML_ResponseTest.java
@@ -0,0 +1,26 @@
+package day10;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import test_util.SpartanWithAuthBaseTest;
+
+import static io.restassured.RestAssured.*;
+
+public class XML_ResponseTest extends SpartanWithAuthBaseTest {
+
+
+     @DisplayName("Test GET /spartans xml response")
+     @Test
+     public void testXML(){
+         // provide credentials and provide header as xml and send request
+         // assert status code 200
+         // assert content type is xml
+         // assert first data name is Wonder Woman
+
+
+
+     }
+
+
+
+}
