2f37e3f821ad258791133f5701cf5b69e3cf5b98
Akbar <akbar@cybertekschool.com>
2021-04-12 11:21:03 -0400

    Day 10 : XmlPath intro

--- src/test/java/day10/XML_ResponseTest.java
+++ src/test/java/day10/XML_ResponseTest.java
@@ -1,6 +1,8 @@
 package day10;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import test_util.SpartanWithAuthBaseTest;
@@ -24,7 +26,7 @@ public class XML_ResponseTest extends SpartanWithAuthBaseTest {
          when()
                  .get("/spartans").
          then()
-                 .log().all()
+                 //.log().all()
                  .statusCode(200)
                  .contentType(ContentType.XML)
                  .body("List.item[0].name" , is("Meade") )
@@ -50,6 +52,29 @@ public class XML_ResponseTest extends SpartanWithAuthBaseTest {
           </item>
     </List>
       */
+     @DisplayName("Test GET /spartans xml response with XmlPath")
+     @Test
+     public void testXML_extractData(){
+
+         Response response = given()
+                 .auth().basic("user","user")
+                 .accept( ContentType.XML ).
+                         when()
+                 .get("/spartans") ;
+
+         XmlPath xp = response.xmlPath() ;
+         int firstId =  xp.getInt("List.item[0].id") ;
+         System.out.println("firstId = " + firstId);
+         String firstName = xp.getString("List.item[0].name") ;
+         System.out.println("firstName = " + firstName);
+
+         long thirdPhoneNumber = xp.getLong("List.item[2].phone") ;
+         System.out.println("thirdPhoneNumber = " + thirdPhoneNumber);
+
+
+     }
+
+
 
 
 
