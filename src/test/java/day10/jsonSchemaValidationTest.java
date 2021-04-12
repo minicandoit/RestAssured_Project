2d0f97fb57eece756dfa54034de601ed5ba68936
Akbar <akbar@cybertekschool.com>
2021-04-12 15:42:28 -0400

    Day 10 : Json Schema Validation for Single Spartan complete

---
+++ src/test/java/day10/jsonSchemaValidationTest.java
@@ -0,0 +1,30 @@
+package day10;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import test_util.SpartanNoAuthBaseTest;
+import static io.restassured.RestAssured.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class jsonSchemaValidationTest extends SpartanNoAuthBaseTest {
+
+
+    @DisplayName("Check GET /spartans/{id} json schema")
+    @Test
+    public void test1SpartanJsonSchema(){
+
+        given()
+                .pathParam("id" , 8005).
+        when()
+                .get("/spartans/{id}").
+        then()
+                .log().body()
+                .statusCode(200)
+                // check the response body against
+                // the schema file singleSpartanSchema.json we added under resources folder
+                .body(  matchesJsonSchemaInClasspath("singleSpartanSchema.json")  )
+        ;
+    }
+
+}
