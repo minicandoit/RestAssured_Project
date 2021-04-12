133430b7125bcce570f5c26af3087a704090793d
Akbar <akbar@cybertekschool.com>
2021-04-12 16:50:51 -0400

    Day 10 : matchesJsonSchemaInClasspath for schema under resources , matchesJsonSchema for schema anywhere by providing full path

--- src/test/java/day10/jsonSchemaValidationTest.java
+++ src/test/java/day10/jsonSchemaValidationTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import test_util.SpartanNoAuthBaseTest;
+
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class jsonSchemaValidationTest extends SpartanNoAuthBaseTest {
 
@@ -36,6 +39,10 @@ public class jsonSchemaValidationTest extends SpartanNoAuthBaseTest {
                 .get("/spartans").
         then()
                 .body(matchesJsonSchemaInClasspath("allSpartansSchema.json")  )
+                // what if this schema file was under day 10 package
+                .body(matchesJsonSchema( new File("src/test/java/day10/allSpartansSchema.json")   ))
+
+
         ;
     }
 
